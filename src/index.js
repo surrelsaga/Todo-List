f7e9c13e07d191eaed49af799baea77090fdc369
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-06-04 02:22:46 +0800

    feat: add a storage for all the Project objects, it'll later be used to save data in the localStorage

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@
 
 // Goal: working todo core logic in console
 import { createProject } from "./createProject.js";
-
+import { makeProjectStorage } from "./projectStorage.js"
---
+++ src/projectStorage.js
@@ -0,0 +1,23 @@
+// IIFE
+export default (function projectStorage() {
+    // array to store Project objects
+    const projectsList = [];
+
+    // Methods to add/remove projects in the list
+    const addProject = (projectObject) => {
+        projectsList.push(projectObject);
+    }
+
+    const removeProject = (projectID) => {
+        // Same approach with toDoList
+        const index = projectsList.findIndex(project => project.getProjectID() === projectID);
+
+        // index == -1 means the project is not inside the list
+        if( index !== -1 ) {
+            // remove the project
+            projectsList.splice(index, 1);
+        }
+    }
+
+    return { addProject, removeProject }
+})();

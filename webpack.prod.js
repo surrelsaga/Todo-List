2b22cc45d2e5eb3a4cb8d172da7d7ea46922be2d
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-06-18 19:53:45 +0800

    fix: path to bundle src code

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 import { merge } from "webpack-merge";
-import common from ".webpack.common.js";
+import common from "./webpack.common.js";
 
 // later combine all config files into one final config (merge)
 export default merge(common, {
